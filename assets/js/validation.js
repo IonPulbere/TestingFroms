57aac3ff248095db641dcaf0e396852024030374
ion <ionPulbere@icloud.com>
2018-06-14 08:51:21 +0100

    modified validation.js

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -2,92 +2,50 @@
 // try to validate the email input
 var item = document.querySelector('.field input[type="email"]');
 
-function CheckIfISEmpty(item,formName){
-// target the form
-var form= document.getElementById('firstForm');
-function validationForm(){
-  var atrname=item.getAttribute("name"); // get the name
-  var x = document.forms[formName][atrname].value
-  if(x==""){
-     var hasP = item.parentElement.childNodes;
-     var exist;
-     var itemexist;
-     for(var i=0; i<hasP.length;i++){
+function CheckIfISEmpty(item, formName) {
+  // target the form
+  var form = document.getElementById('firstForm');
 
-       if(hasP[i].nodeName.toUpperCase()=='P'){
-
-         exist=true;
-         itemexist=hasP[i];
-       } else{
-         exist=false
-
-
-       }
-     }
-        // if(exist){ // here ledtf
-          // var existingPara= document.getElementById("tempo");
-          // if (itemexist) {
-          //   // console.log('ss');
-          //   existingPara.parentNode.removeChild(itemexist); //remove the child created
-          //     }
-          // make it toUpperCase and concatenate
-          // var s1=atrname.substring(0,1).toUpperCase();
-          //     atrname=s1+atrname.substring(1);
-          // var neP= document.createElement("p");
-          // var neText= document.createTextNode(atrname+" field is required !");
-          // neP.appendChild(neText);
-          // neP.className="validate-me";
-          // neP.id='tempo';
-          // item.parentElement.appendChild(neP);
-
-           // return false;
-         // } else{
-           var s1=atrname.substring(0,1).toUpperCase();
-               atrname=s1+atrname.substring(1);
-           var neP= document.createElement("p");
-           var neText= document.createTextNode(atrname+" field is required !");
-           neP.appendChild(neText);
-           neP.className="validate-me";
-           neP.id='tempo';
-           item.parentElement.appendChild(neP);
-         // }
-
-  } else{
-    var existingPara= document.getElementById("tempo");
-    if (existingPara) {
-      existingPara.parentNode.removeChild(existingPara); //remove the child created
+  function validationForm() {
+    var atrname = item.getAttribute("name"); // get the name
+    var x = document.forms[formName][atrname].value
+    if (x == "") {
+      var hasP = item.parentElement.childNodes;
+      // Check the field has already a p tag
+      for (var i = 0; i < hasP.length; i++) {
+        if (hasP[i].nodeName.toUpperCase() == 'P') {
+          console.log((hasP[i]).parentNode);
+          (hasP[i]).parentNode.removeChild(hasP[i]); //remove the p tag
         }
       }
-}
-// validationForm();
-item.addEventListener('blur',function(){
-validationForm();
-},false);
-} // end of function
-
-//  Check the specified field
-var listOfRequired=[ 'color','date','number','url', 'email','datetime','as'];
-for(var i =0 ; i<listOfRequired.length; i++){
-  var item = document.querySelector('.field input[type='+listOfRequired[i]+']');
-  if(item){
-    CheckIfISEmpty(item,'formName');
+      var s1 = atrname.substring(0, 1).toUpperCase();
+      atrname = s1 + atrname.substring(1);
+      var neP = document.createElement("p");
+      var neText = document.createTextNode(atrname + " field is required !");
+      neP.appendChild(neText);
+      neP.className = "validate-me";
+      neP.id = 'tempo';
+      item.parentElement.appendChild(neP);
+    } else {
+      var existingPara = document.getElementById("tempo");
+      if (existingPara) {
+        existingPara.parentNode.removeChild(existingPara); //remove the child created
+      }
     }
+  }
+  // validationForm();
+  item.addEventListener('blur', function() {
+    validationForm();
+  }, false);
+} // end of function
+//  Check the specified field
+var listOfRequired = ['color', 'date', 'number', 'url', 'email', 'datetime', 'as'];
+for (var i = 0; i < listOfRequired.length; i++) {
+  var item = document.querySelector('.field input[type=' + listOfRequired[i] + ']');
+  if (item) {
+    CheckIfISEmpty(item, 'formName');
+  }
 }
-
-// go through all elemnts in form
-// document.querySelectorAll("p");
-// var form= document.getElementById('firstForm');
-// var formChildren=form.childNodes;
-// for (var i=0;i<formChildren.length;i++){
-// console.log(formChildren[i].child);
-// }
-// for ( var f in form){
-//
-// }
-
-
-
-
 
 // +++++++++++++++++++
 
